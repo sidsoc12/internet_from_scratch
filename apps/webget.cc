926ebb100c67ffdffb069160bee5f3130ebb5e68
Sid Potti <sidpotti@stanford.edu>
2025-01-15 23:22:44 +0000

    commented out initial err code

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,9 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
