a372a65b99abfd54cebbabf863aa2db4ffc6c953
Sid Potti <sidpotti@stanford.edu>
2025-01-23 00:31:56 +0000

    fixed stuff in merge

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -18,13 +18,38 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   if(first_index >= next_index + available_capacity){ // data is entirely beyond capacity
     return;
   }
-  
 
 
   // Conduct merge with existing buffer
   
   uint64_t start = first_index;
   uint64_t end = first_index + data.size();
+  std::string merged_data = data;
+
+  auto it = reassembler_buffer.lower_bound(first_index);
+  if( it != reassembler_buffer.begin()){ // find the entry slightly higher than substring index
+    --it;  // get entry right before substring index (start merge with that)
+  }
+
+  // iterating through all the possible merges 
+  while(it -> first < end && it != reassembler_buffer.end() ){
+    uint64_t buff_start = it->first;
+    uint64_t buff_end = it->first + it->second.size();
+    if(buff_start <= end && buff_end >= start){
+      start = std::min(start, buff_start);
+      end = std::max(end, buff_end);
+      merged_data.replace(buff_start - start, it->second.size(), it->second);
+      // Remove buffered sring 
+      it = reassembler_buffer.erase(it);
+    }
+    else{
+      break;
+    }
+  }
+
+
+
+
 
 
 
