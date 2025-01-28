1c322bf9c8d39c6b1393eccf7846939fa2f60a13
Sid Potti <sidpotti@stanford.edu>
2025-01-28 08:23:41 +0000

    added base

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -12,6 +12,9 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
+  // Calculate Base
+  uint64_t base = (raw_value_ + ((uint64_t(1) << 32) - zero_point.raw_value_)) % (uint64_t(1) << 32);
   
+
   return {};
 }
