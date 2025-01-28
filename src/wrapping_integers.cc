a67b18f43817184e275cf4635469d3df35a3e3f5
Sid Potti <sidpotti@stanford.edu>
2025-01-28 08:38:30 +0000

    added unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,10 +11,16 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  // Calculate Base
+   // Calculate base
   uint64_t base = (raw_value_ + ((uint64_t(1) << 32) - zero_point.raw_value_)) % (uint64_t(1) << 32);
-  
-
-  return {};
+  uint64_t before_checkpoint = base + ((checkpoint - base) / (1UL << 32)) * (1UL << 32);
+  uint64_t after_checkpoint = before_checkpoint + (1UL << 32);
+  // find candidates right before and after checkpoint
+  uint64_t diff_before = checkpoint - before_checkpoint;
+  uint64_t diff_after = after_checkpoint - checkpoint;
+  // Compare candidates 
+  if(diff_before > diff_after){
+    return after_checkpoint;
+  }
+  return before_checkpoint;
 }
