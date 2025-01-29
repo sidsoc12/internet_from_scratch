508697a7d5e00884c0233268394e69812d1961c7
Sid Potti <sidpotti@stanford.edu>
2025-01-29 04:07:25 +0000

    added more edge cases to recieve and implemented send

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,7 +6,6 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
   if(message.RST){ // handle RST 
     rst = true;
     return;
@@ -15,30 +14,43 @@ void TCPReceiver::receive( TCPSenderMessage message )
     ISN = message.seqno;
     received_SYN = true;
   }
+  else if(received_SYN && message.SYN){ // Ignoring duplicate SYN's
+    return; 
+  }
   if(!received_SYN){ // If there is no start sequence, we cannot do anything
     return; 
   }
   if(message.SYN == false && message.payload.empty() && !message.FIN){
     return;
   }
+  // No data to push yet => just update ISN and return
+  if (message.SYN && message.payload.empty() && !message.FIN) {
+    return;  
+  }
   // Steps for pushing data to reassembler 
   uint64_t absolute_seqn = message.seqno.unwrap(*ISN, checkpoint);
-  uint64_t reassembler_index = absolute_seqn - (message.SYN ? 0 : 1); // getting rid of SYN index
+  uint64_t reassembler_index = absolute_seqn - 1; // getting rid of SYN index
 
   // insert into reassembler 
   reassembler_.insert(reassembler_index, message.payload, message.FIN);
-  checkpoint = reassembler_index + message.payload.size();
+  checkpoint = reassembler_index + message.payload.size() - (message.FIN ? 1 : 0);
   // Update FIN flag 
   if(message.FIN){
     received_FIN = true;
   }
-  ackno = *ISN + checkpoint + (received_FIN ? 1 : 0);
-  window_size = std::min(static_cast<uint16_t>(reassembler_.writer().available_capacity()), UINT16_MAX);
+  ackno = *ISN + checkpoint + (received_FIN ? 1 : 0) + 1; //have to add 1 to add back the SYN byte
+  window_size = static_cast<uint16_t>(std::min(reassembler_.writer().available_capacity(), static_cast<size_t>(UINT16_MAX)));
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  
-  return {};
+  if (rst) {
+        return { std::nullopt, 0, true };
+  }
+  // If no SYN was received, we have no valid ackno
+  if (!ackno) {
+        return { std::nullopt, 0, false };
+  }
+  // Return stored values (no recomputation)
+  return { ackno, window_size, false };
 }
