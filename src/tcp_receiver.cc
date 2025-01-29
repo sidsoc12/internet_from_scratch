4bbb2ad77cf14457d707230976021fda62b24ac9
Sid Potti <sidpotti@stanford.edu>
2025-01-29 00:47:48 +0000

    fixed underflow+overflow errors

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,13 +6,13 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  debug( "unimplemented receive() called" );
+  
   (void)message;
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  debug( "unimplemented send() called" );
+  
   return {};
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,18 +6,19 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  return Wrap32 {(n + zero_point.raw_value_) % 1UL << 32}; // 1 as a unsigned integer (1UL)
+  return Wrap32{static_cast<uint32_t>((n % (1ULL << 32) + zero_point.raw_value_) % (1ULL << 32))}; // 1 as a unsigned integer (1UL)
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
    // Calculate base
   uint64_t base = (raw_value_ + ((uint64_t(1) << 32) - zero_point.raw_value_)) % (uint64_t(1) << 32);
-  uint64_t before_checkpoint = base + ((checkpoint - base) / (1UL << 32)) * (1UL << 32);
-  uint64_t after_checkpoint = before_checkpoint + (1UL << 32);
+  uint64_t before_checkpoint = base + ((static_cast<int64_t>(checkpoint) - static_cast<int64_t>(base)) / (uint64_t(1)  << 32)) * (uint64_t(1)  << 32);
+  uint64_t after_checkpoint = before_checkpoint + (uint64_t(1)  << 32);
   // find candidates right before and after checkpoint
-  uint64_t diff_before = checkpoint - before_checkpoint;
-  uint64_t diff_after = after_checkpoint - checkpoint;
+  uint64_t diff_before = (checkpoint > before_checkpoint) ? checkpoint - before_checkpoint : before_checkpoint - checkpoint;
+  uint64_t diff_after = (after_checkpoint > checkpoint) ? after_checkpoint - checkpoint : checkpoint - after_checkpoint;
+
   // Compare candidates 
   if(diff_before > diff_after){
     return after_checkpoint;
